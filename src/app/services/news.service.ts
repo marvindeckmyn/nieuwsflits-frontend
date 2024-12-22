1433a382ab89a0a455ba53cd59dadb84a71c53e4
marvindeckmyn <marvindeckmyn@gmail.com>
2024-12-22 23:38:05 +0100

    Add NewsService

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { NewsArticle } from "../models/news-article.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NewsService {
+    private apiUrl = 'https://api.nieuwsflits.be/api/news';
+
+    constructor(private http: HttpClient) { }
+
+    getLatestNews(): Observable<NewsArticle[]> {
+        return this.http.get<NewsArticle[]>(`${this.apiUrl}/latest`);
+    }
+
+    getArticleById(id: number): Observable<NewsArticle> {
+        return this.http.get<NewsArticle>(`${this.apiUrl}/${id}`);
+    }
+
+    getNewsByCategory(category: string): Observable<NewsArticle[]> {
+        return this.http.get<NewsArticle[]>(`${this.apiUrl}/category/${category}`);
+    }
+}
